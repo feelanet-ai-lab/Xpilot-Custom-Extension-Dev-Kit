2b66fa2356c4c7b10525f5eb6d49eea57ce14c6b
jychoi <wipsilon480@gmail.com>
2025-09-02 17:34:46 +0900

    ngrok, mcp 설치 및 common libary 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { HttpModule } from "@nestjs/axios";
+import { Module } from "@nestjs/common";
+import { McpModule, McpTransportType } from "@rekog/mcp-nest";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    McpModule.forRoot({
+      name: process.env.SERVER_NAME,
+      version: process.env.SERVER_VERSION,
+      transport: McpTransportType.STREAMABLE_HTTP,
+      streamableHttp: {
+        enableJsonResponse: true,
+        sessionIdGenerator: undefined,
+        statelessMode: true, // No session management
+      },
+    }),
+    HttpModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { loadConfiguration } from "@lib/common-library";
+import * as Joi from "joi";
+
+// ==============================================================================
+
+const joiConfigSchemaValidation = Joi.object({
+  MODE: Joi.string().trim().valid("local", "prod").required(),
+
+  EXPOSE_PORT: Joi.number().port().required(),
+
+  NGROK_ACCESS_KEY: Joi.when("MODE", {
+    is: "local",
+    then: Joi.string().trim().required(),
+    otherwise: Joi.string().trim().allow("").optional(),
+  }),
+
+  SERVER_NAME: Joi.string().trim().required(),
+  SERVER_DESCRIPTION: Joi.string().trim().required(),
+  SERVER_VERSION: Joi.string().trim().required(),
+
+  // --------------------------------------------------------------------------
+});
+
+loadConfiguration("config.json", joiConfigSchemaValidation);
+
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.EXPOSE_PORT || 3000);
 }
 bootstrap();
